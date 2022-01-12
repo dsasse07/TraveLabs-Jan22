72c9575495ba1f66314dabd0482cea0092c76ef9
Daniel Sasse <dsasse07@gmail.com>
2022-01-12 13:53:41 -0500

    finished 42-44 project 10

---
+++ 100-days-of-swift/042-45-project10-NamesFaces/Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Daniel Sasse on 1/12/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    // NSObject is the base of all UIKit objects. Not required, but provides access to ObjC methods
+    
+    var name: String
+    var imageName: String
+    
+    init(name: String, imageName: String){
+        self.name = name
+        self.imageName = imageName
+    }
+}
---
+++ 100-days-of-swift/042-45-project10-NamesFaces/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Daniel Sasse on 1/12/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
